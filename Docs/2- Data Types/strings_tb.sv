68116362b86f099ba9576879e3524940c7704b27
Choaib ELMADI <choaibamd@gmail.com>
2025-10-28 11:40:13 +0100

    SystemVerilog strings

---
+++ Docs/2- Data Types/strings_tb.sv
@@ -0,0 +1,14 @@
+module strings_tb;
+
+    string dialog = "Hello!";
+
+    initial begin
+
+        $display("%s", dialog);
+        foreach (dialog[i]) begin
+            $display("%s", dialog[i]);
+        end
+
+    end
+
+endmodule
